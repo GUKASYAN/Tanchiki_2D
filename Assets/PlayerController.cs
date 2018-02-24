ba037504c5bca316429b52be95e3055ce533156a
Igor Gukasyan <GUKASYAN@users.noreply.github.com>
2018-02-24 12:37:28 +0200

    add: texures, navMesh

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform Target;
+    public NavMeshAgent _navMeshAgent;
+
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        _navMeshAgent.SetDestination(Target.position);
+    }
+}
